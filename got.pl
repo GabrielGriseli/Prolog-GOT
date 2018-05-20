bfc98409def6ec801a53659ea20302d0fefd4640
Gabriel Júnior Griseli <gabrielgriseli@gmail.com>
2018-05-20 16:51:53 -0300

    Initial commit

---
+++ got.pl
@@ -0,0 +1,118 @@
+/* facts from the family tree of the Stark family */  
+
+parent(rickard_stark, brandon_stark).
+parent(rickard_stark, eddard_stark).
+parent(rickard_stark, benjen_stark).
+parent(rickard_stark, lyanna_stark).
+/**/
+parent(eddard_stark, robb_stark).
+parent(eddard_stark, sansa_stark).
+parent(eddard_stark, arya_stark).
+parent(eddard_stark, bran_stark).
+parent(eddard_stark, rickon_stark).
+parent(lyanna_stark, jon_snow).
+
+/* facts from the family tree of the Targaryen family */
+
+/* parent(aemon_targaryen, NO SON). */
+/**/
+parent(aegon_v_targaryen, duncan_targaryen).
+parent(aegon_v_targaryen, aerys_ii_targaryen).
+parent(aegon_v_targaryen, rhaella_targaryen).
+parent(aegon_v_targaryen, daeron_targaryen).
+/**/
+/* parent(aerion_targaryen, NO SON). */
+/**/
+parent(aerys_ii_targaryen, rhaegar_targaryen).
+parent(aerys_ii_targaryen, viserys_targaryen).
+parent(aerys_ii_targaryen, daenerys_targaryen).
+/**/
+parent(rhaella_targaryen, rhaegar_targaryen).
+parent(rhaella_targaryen, viserys_targaryen).
+parent(rhaella_targaryen, daenerys_targaryen).
+/**/
+parent(rhaegar_targaryen, jon_snow).
+parent(rhaegar_targaryen, rhaenys_targaryen).
+parent(rhaegar_targaryen, aegon_targaryen).
+
+/* facts from the family tree of the Lannister family */
+
+parent(tytos_lannister, tywin_lannister).
+parent(tytos_lannister, kevan_lannister).
+/**/
+parent(tywin_lannister, cersei_lannister).
+parent(tywin_lannister, jaime_lannister).
+parent(tywin_lannister, tyrion_lannister).
+/**/
+parent(kevan_lannister, lancel_lannister).
+parent(kevan_lannister, willem_lannister).
+parent(kevan_lannister, martyn_lannister).
+/**/
+parent(cersei_lannister, joffrey_lannister).
+parent(cersei_lannister, myrcella_lannister).
+parent(cersei_lannister, tommen_lannister).
+/**/
+parent(jaime_lannister, joffrey_lannister).
+parent(jaime_lannister, myrcella_lannister).
+parent(jaime_lannister, tommen_lannister).
+
+/* facts from the family tree of the Greyjoy family */
+
+parent(balon_greyjoy, rodrik_greyjoy).
+parent(balon_greyjoy, maron_greyjoy).
+parent(balon_greyjoy, yara_greyjoy).
+parent(balon_greyjoy, theon_greyjoy).
+/**/
+/* parent(euron_greyjoy, NO SON).
+/**/
+/* parent(aeron_greyjoy, NO SON).
+
+/* facts from the family tree of the Baratheon family */
+
+parent(steffon_baratheon, robert_baratheon).
+parent(steffon_baratheon, stannis_baratheon).
+parent(steffon_baratheon, renly_baratheon).
+/**/
+parent(robert_baratheon, gendry).
+/**/
+parent(stannis_baratheon, shireen_baratheon).
+
+/* facts from the family tree of the Tyrell family */
+
+parent(luthor_tyrell, mace_tyrell).
+/**/
+parent(mace_tyrell, margaery_tyrell).
+parent(mace_tyrell, loras_tyrell).
+
+/* facts from the family tree of the Martell family */
+
+parent(lewyn_martell, doran_martell).
+parent(lewyn_martell, elia_martell).
+parent(lewyn_martell, oberyn_martell).
+/**/
+parent(doran_martell, trystane_martell).
+/**/
+parent(elia_martell, rhaenys_targaryen).
+parent(elia_martell, aegon_targaryen).
+/**/
+parent(oberyn_martell, tyene_sand).
+parent(oberyn_martell, obara_sand).
+parent(oberyn_martell, sarella_sand).
+parent(oberyn_martell, nymeria_sand).
+
+/* facts from the family tree of the Tully family */
+
+parent(robbyrt_tully, hoster_tully).
+parent(robbyrt_tully, brynden_tully).
+/**/
+parent(hoster_tully, catelyn_tully).
+parent(hoster_tully, lysa_tully).
+parent(hoster_tully, edmure_tully).
+
+/* facts from the family tree of the Arryn family */
+
+parent(jasper_arryn, jon_arryn).
+parent(jasper_arryn, alys_arryn).
+parent(jasper_arryn, ronnel_arryn).
+/**/
+parent(jon_arryn, robin_arryn).
